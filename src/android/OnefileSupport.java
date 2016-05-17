a4a50d3df11f1b91c5fca90f17bc1f2c5a9a1a61
Ben Marshall <ben3005@hotmail.com>
2016-05-17 21:30:10 +0100

    added draft http utility, changed arguments, probably broken

--- src/android/OnefileSupport.java
+++ src/android/OnefileSupport.java
@@ -10,6 +10,8 @@ import org.json.JSONException;
 import org.json.JSONObject;
 import android.util.Log;
 
+import java.net.HttpURLConnection;
+
 public class OnefileSupport extends CordovaPlugin {
 	
 	@Override
@@ -22,22 +24,48 @@ public class OnefileSupport extends CordovaPlugin {
 	public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
 		Log.i("OneFileSupportPlugin", "Im in here!");
 		if (action.equals("onefileSupport")) {
-			if (args.length() != 3) {
-				callbackContext.error("Expected 3 arguments");
-			}
-			this.uploadSupport(args.getString(0), args.getString(1), args.getString(2), callbackContext);
+			JSONObject config = args.getJSONObject(0);
+			String ticketDescription = config.getString("ticketDescription");
+			String ticketNumber = config.getString("ticketNumber");
+			String contactDetails = config.getString("contactDetails");
+			JSONArray files = config.getJSONArray("files");
+			String sessionToken = config.getString("sessionToken");
+			String endpoint = config.getString("endpoint");
+			callbackContext.success(config);
+			//this.uploadSupport(args.getString(0), args.getString(1), args.getString(2), callbackContext);
 			return true;
 		}
 		return false;
 	}
 	
-	private void uploadSupport(String ticketDescription, String ticketNumber, String contactDetails, CallbackContext callbackContext) {
+	private void uploadSupport(String requestURL, , CallbackContext callbackContext) {
 		JSONObject retObj = new JSONObject();
 		try {
 			retObj.put("ticketDescription", ticketDescription);
 			retObj.put("ticketNumber", ticketNumber);
 			retObj.put("contactDetails", contactDetails);
 			callbackContext.success(retObj);
+			
+			
+			/*MultipartUtility multipart = new MultipartUtility(requestURL, "UTF-8");
+
+			for (int i = 0; i < myFormDataArray.size(); i++) {
+				multipart.addFormField(myFormDataArray.get(i).getParamName(),
+						myFormDataArray.get(i).getParamValue());
+			}
+
+			
+			for (int i = 0; i < myFileArray.size(); i++) {
+				multipart.addFilePart(myFileArray.getParamName(),
+						new File(myFileArray.getFileName()));
+			}
+
+			List<String> response = multipart.finish();
+			Debug.e(TAG, "SERVER REPLIED:");
+			for (String line : response) {
+				responseString = line;
+			}*/
+			
 		} catch (JSONException e) {
 			callbackContext.error("error");
 		} 
--- www/onefileSupport.js
+++ www/onefileSupport.js
@@ -1,8 +1,24 @@
-window.support = function(ticketDescription, ticketNumber, contactDetails, success, error) {
-	var args = [
-		ticketDescription,
-		ticketNumber,
-		contactDetails
-	];
-    cordova.exec(success, error, "OnefileSupport", "onefileSupport", args);
+window.support = function(config, success, error) {
+	if (!config.ticketDescription) {
+		error('Missing ticket description');
+	}
+	if (!config.contactDetails) {
+		error('Missing contact details');
+	}
+	if (!config.ticketNumber) {
+		error('Missing ticket number');
+	}
+	if (!config.sessionToken) {
+		error('Missing session token');
+	}
+	if (!config.endpoint) {
+		error('Missing endpoint');
+	}
+	if (!config.files) {
+		error('Missing files');
+	}
+	if (config.files && config.files.length === 0) {
+		error('Missing files');
+	}
+    cordova.exec(success, error, "OnefileSupport", "onefileSupport", [config]);
 };
