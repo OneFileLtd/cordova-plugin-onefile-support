f4f06a381b5af1080b5cd69c1e65026f4931254c
Coral Smith <coral.smith92@gmail.com>
2017-03-31 10:39:45 +0100

    Fix my inability to check changes before commits - remove code required to run locally

--- src/windows/ZipFile/ZipFile/js/OnefileSupport.js
+++ src/windows/ZipFile/ZipFile/js/OnefileSupport.js
@@ -273,17 +273,19 @@
 	}
 
 	// To run this from the solution, comment out the line below
-    // require("cordova/exec/proxy").add("OnefileSupport", OnefileSupportProxy);
-	OnefileSupportProxy.onefileRecover(function s() {
-	    console.log('success');
-	}, function e(err) {
-	    console.log(err);
-	}, [{
-	    endpoint: "https://wsapiuat2.onefile.co.uk/api/v2/Mobile/DisasterRecovery",
-	    sessionToken: "sQcK6f18oqcakBHSzFhimmNhyP4/DBcxYL0ZvuouuCtNRyku+iZS13ZG5HCJoQOjooa9E3uN1Br+e/EKeRmDrKN7jU2bDCONzqo4SAjw5dVUPU/zrFW9ZYaZJ7y13Lke0JcLN0v87Yu//eau3zNLmIv79UFS4GJ3X2qpEawd/MwGcLGVFQEjyL4+2N1MH8LvqTXkhFl3XE6TJ01m4zJJA2HmRLI4YlemVWWt63O0oe3SqVFPKHl7YFyIFLG/fewH1XTDjdw+u2HSrxN07HBhjiqFSkeKoJv12Xzz2BEqRblPOZy0+j76LK2OxWO7ZgrIHva5RaTVC6KCilQ9pTuPnA==",
-	    maxFileSize: 104857600,
-	    maxZipFiles: 10,
-	    selectedServer: 6,
-        ticketNumber: "123456789 d"
-	}]);
+	require("cordova/exec/proxy").add("OnefileSupport", OnefileSupportProxy);
+
+    // Uncomment this to run locally
+	//OnefileSupportProxy.onefileRecover(function s() {
+	//    console.log('success');
+	//}, function e(err) {
+	//    console.log(err);
+	//}, [{
+	//    endpoint: "https://wsapiuat2.onefile.co.uk/api/v2/Mobile/DisasterRecovery",
+	//    sessionToken: "sQcK6f18oqcakBHSzFhimmNhyP4/DBcxYL0ZvuouuCtNRyku+iZS13ZG5HCJoQOjooa9E3uN1Br+e/EKeRmDrKN7jU2bDCONzqo4SAjw5dVUPU/zrFW9ZYaZJ7y13Lke0JcLN0v87Yu//eau3zNLmIv79UFS4GJ3X2qpEawd/MwGcLGVFQEjyL4+2N1MH8LvqTXkhFl3XE6TJ01m4zJJA2HmRLI4YlemVWWt63O0oe3SqVFPKHl7YFyIFLG/fewH1XTDjdw+u2HSrxN07HBhjiqFSkeKoJv12Xzz2BEqRblPOZy0+j76LK2OxWO7ZgrIHva5RaTVC6KCilQ9pTuPnA==",
+	//    maxFileSize: 104857600,
+	//    maxZipFiles: 10,
+	//    selectedServer: 6,
+    //    ticketNumber: "123456789 d"
+	//}]);
 })();
